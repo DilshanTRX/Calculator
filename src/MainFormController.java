12793c82be7971cd66381c7a723b6a39211d35f8
DilshanTRX <dilshantrx@gmail.com>
2022-11-05 16:50:50 +0530

    Initial commit

---
+++ src/MainFormController.java
@@ -0,0 +1,40 @@
+import javafx.scene.input.MouseEvent;
+
+public class MainFormController {
+
+    public void num1OnAction(MouseEvent mouseEvent) {
+    }
+
+    public void num2OnAction(MouseEvent mouseEvent) {
+    }
+
+    public void num5OnAction(MouseEvent mouseEvent) {
+    }
+
+    public void num4OnAction(MouseEvent mouseEvent) {
+    }
+
+    public void num3OnAction(MouseEvent mouseEvent) {
+    }
+
+    public void num6OnAction(MouseEvent mouseEvent) {
+    }
+
+    public void num7OnAction(MouseEvent mouseEvent) {
+    }
+
+    public void num8OnAction(MouseEvent mouseEvent) {
+    }
+
+    public void EqOnAction(MouseEvent mouseEvent) {
+    }
+
+    public void num0OnAction(MouseEvent mouseEvent) {
+    }
+
+    public void num9OnAction(MouseEvent mouseEvent) {
+    }
+
+    public void MulOnAction(MouseEvent mouseEvent) {
+    }
+}
